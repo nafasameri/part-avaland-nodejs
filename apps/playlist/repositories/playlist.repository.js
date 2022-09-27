1f4432e15f336fd29dd96cf411da28eb0edf8089
Nafas <38180578+nafasameri@users.noreply.github.com>
2022-09-27 21:24:24 +0330

    change model of playlist

--- apps/playlist/repositories/playlist.repository.js
+++ apps/playlist/repositories/playlist.repository.js
@@ -6,42 +6,50 @@ const datetime = require('../../../modules/utility').datetime;
 class PlaylistRepository {
     async fetchAll() {
         const record = await db.selcet('Playlist', '*');
-        return record.rows;
+        const playlists = [];
+        record.rows.forEach((row) => {
+            playlists.push(new Playlist(row).get());
+        });
+        return playlists;
     }
 
     async fetchById(id) {
         const record = await db.selcet('Playlist', '*', `"PlaylistID"=${id}`);
-        return record.rows[0];
+        return record.rows[0] ? new Playlist(record.rows[0]).get() : undefined;
     }
 
     async add(playlist, userID) {
-        const playlistModel = new Playlist(
-            0,
-            playlist.PlaylistName ?? '',
-            playlist.PlaylistDesc ?? '',
-            userID,
-            datetime(),
-            userID,
-            datetime(),
-            0
-        );
-        const record = await db.insert('Playlist', '"PlaylistName", "PlaylistDesc", "Creator", "CreateTime", "Modifier", "ModifiTime", "IsDelete"',
-            `'${playlistModel.PlaylistName}', '${playlistModel.PlaylistDesc}', ${playlistModel.Creator}, '${playlistModel.CreateTime}', ${playlistModel.Modifier}, '${playlistModel.ModifiTime}', ${playlistModel.IsDelete}`);
-        return record.rows[0];
+        const playlistModel = {
+            PlaylistName: playlist.name ?? '',
+            PlaylistImg: playlist.img ?? '',
+            Creator: userID ?? null,
+            CreateTime: datetime(),
+            Modifier: userID ?? null,
+            ModifiTime: datetime(),
+            IsDelete: 0
+        };
+        const record = await db.insert('Playlist', '"PlaylistName", "PlaylistImg", "Creator", "CreateTime", "Modifier", "ModifiTime", "IsDelete"',
+            `'${playlistModel.PlaylistName}', '${playlistModel.PlaylistImg}', ${playlistModel.Creator}, '${playlistModel.CreateTime}', ${playlistModel.Modifier}, '${playlistModel.ModifiTime}', ${playlistModel.IsDelete}`);
+        return record.rows[0] ? new Playlist(record.rows[0]).get() : undefined;
     }
 
     async update(playlist, userID) {
-        playlist.Modifier = userID;
-        playlist.ModifiTime = datetime();
+        const playlistModel = {
+            PlaylistID: playlist["playlist-id"] ?? null,
+            PlaylistName: playlist.name ?? '',
+            PlaylistImg: playlist.img ?? '',
+            Modifier: userID ?? null,
+            ModifiTime: datetime(),
+        };
 
-        const record = await db.update('Playlist', `"PlaylistName"='${playlist.PlaylistName}', "PlaylistDesc"='${playlist.PlaylistDesc}', "Modifier"=${playlist.Modifier}, "ModifiTime"='${playlist.ModifiTime}'`,
-            `"PlaylistID"=${playlist.PlaylistID}`);
-        return record.rows[0];
+        const record = await db.update('Playlist', `"PlaylistName"='${playlistModel.PlaylistName}', "PlaylistImg"='${playlistModel.PlaylistImg}', "Modifier"=${playlistModel.Modifier}, "ModifiTime"='${playlistModel.ModifiTime}'`,
+            `"PlaylistID"=${playlistModel.PlaylistID}`);
+        return record.rows[0] ? new Playlist(record.rows[0]).get() : undefined;
     }
 
     async delete(id, userID) {
         const record = await db.update('Playlist', `"Modifier"=${userID}, "ModifiTime"='${datetime()}', "IsDelete" = 1`, `"PlaylistID"=${id}`);
-        return record.rows[0];
+        return record.rows[0] ? new Playlist(record.rows[0]).get() : undefined;
     }
 }
 
