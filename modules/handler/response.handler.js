7276e1fa79aadc23e85b59117ed4c7d87c7f70c3
Nafas <nafiseh.ameri@partsoftware.com>
2022-10-07 23:59:33 +0330

    fix: change port for dist front

--- modules/handler/response.handler.js
+++ modules/handler/response.handler.js
@@ -2,7 +2,7 @@ const statusCode = require('http-status-codes');
 const { datetime } = require('../utility');
 
 module.exports = (res, status, header, content) => {
-    header['Access-Control-Allow-Origin'] = 'http://localhost:8080';
+    header['Access-Control-Allow-Origin'] = 'http://localhost:3000';
     header['Access-Control-Allow-Credentials'] = true;
     header['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE';
     header['Access-Control-Allow-Headers'] = '*';
